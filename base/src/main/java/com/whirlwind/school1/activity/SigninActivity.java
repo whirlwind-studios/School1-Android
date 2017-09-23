1be2c4d3ef22dc59b0ca18dea03979558e091a39
Leon Höpfl <leon.hoepfl.p@gmail.com>
2017-09-23 10:45:34 +0200

    Merged specific signin activities to one with multiple signin options

--- base/src/main/java/com/whirlwind/school1/activity/SigninActivity.java
+++ base/src/main/java/com/whirlwind/school1/activity/SigninActivity.java
@@ -1,15 +1,25 @@
 package com.whirlwind.school1.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.whirlwind.school1.R;
 import com.whirlwind.school1.base.BaseActivity;
 
 public class SigninActivity extends BaseActivity {
 
+    Button logoutButton, anonymousButton, googleButton, facebookButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        anonymousButton = findViewById(R.id.activity_signin_button_anonymous);
+        googleButton = findViewById(R.id.activity_signin_button_google);
+        facebookButton = findViewById(R.id.activity_signin_button_facebook);
+
+        //logoutButton=findViewById(R.id.activity_signin_button_logout);
+        //logoutButton.setVisibility(View.GONE);
     }
 }
